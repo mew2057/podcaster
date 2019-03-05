7bac562fc51945405c3126a29fbd0c5aada70e14
John Dunham <jfdbear1@gmail.com>
2019-03-04 22:20:58 -0500

    Added a back override to card logic.

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,15 @@
+var ActiveElement=null;
+
+function initEvents()
+{
+    window.onhashchange = function() {
+        if ( ActiveElement !== null )
+        {
+            hideElements(ActiveElement);
+        }
+    }
+}
+
 function expandElement(id)
 {
     var element = document.getElementById(id)
@@ -29,12 +41,20 @@ function expandElement(id)
         element = document.getElementById(id + "-close")
         element.classList.add("description-visible")     
         
+        window.location.href = "#"+ id + "-card"
         window.scrollTo(0,0);
+        
+        $("#" + id + "-card").one($.support.transition.end,
+            function() {
+                ActiveElement=id;
+        });
     }
 }
 
 function hideElements(id)
 {
+    ActiveElement=null;
+
     $('.card-1').show();
     $('.about-me').show();
 
@@ -42,18 +62,17 @@ function hideElements(id)
     if ( elements &&  elements.length > 0 )
     {
         for (var i=elements.length-1; i >=0; i--) {
-            elements[i].classList.remove("description-visible")
+            elements[i].classList.remove("description-visible");
         }
     }
-    var element = document.getElementById(id)
-    element.classList.add("just-hid")
-
+    var element = document.getElementById(id);
+    element.classList.add("just-hid");
+    window.location.href = "#"+ id + "-card"
 
     $("#" + id + "-card").one($.support.transition.end,
         function() {
-            document.getElementById(id + "-card").scrollIntoView({behavior: "smooth"})
+            document.getElementById(id + "-card").scrollIntoView({behavior: "smooth"});
 
     });
-
 
 }
